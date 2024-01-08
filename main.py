bb4894b3857f7812c677ca83e516bbc652059ba8
zarusoba <zarusoba327@gmail.com>
2024-01-08 21:15:13 +0900

    asdasds

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import os
 
-app = Flask(__name__, static_folder='./templates/images', static_url_path='/static')
+app = Flask(__name__, static_folder='./templates/images')
 
 
 @app.route('/')
